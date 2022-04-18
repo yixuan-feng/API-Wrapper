be720f311081c0334eacb66cd061dd1f11c8e4af
faithNassiwa <faithnassiwa@gmail.com>
2022-04-18 14:20:12 -0400

    Update to tests to include setUp objects

--- tests/tests.py
+++ tests/tests.py
@@ -1,27 +1,33 @@
 import unittest
-import api_wrapper
+from api_wrapper import PublicApi, ApiWrapper
 
 
 class TestPublicApi(unittest.TestCase):
+    def setUp(self):  # runs before every single test
+        self.api_dog_facts = PublicApi("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all")
+        self.api_covid_19 = PublicApi("https://covid-api.mmediagroup.fr/v1/", "vaccines")
+
+    def tearDown(self):  # runs after every single test
+        pass
+
     def test_get_base_url(self):
-        api = api_wrapper.PublicApi("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all")
-        self.assertEqual(api.get_base_url(), "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all")
+        self.assertEqual(self.api_dog_facts.get_base_url(),
+                         "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all")
 
     def test_get_endpoint(self):
-        api = api_wrapper.PublicApi("https://covid-api.mmediagroup.fr/v1/", "vaccines")
-        self.assertEqual(api.get_endpoint(), "vaccines")
+        self.assertEqual(self.api_covid_19.get_endpoint(), "vaccines")
 
     def test_set_base_url(self):
-        api = api_wrapper.PublicApi("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all")
-        api.set_base_url("https://covid-api.mmediagroup.fr/v1/")
-        self.assertEqual(api.get_base_url(), "https://covid-api.mmediagroup.fr/v1/")
+        self.api_dog_facts.set_base_url("https://covid-api.mmediagroup.fr/v1/")
+        self.assertEqual(self.api_dog_facts.get_base_url(), "https://covid-api.mmediagroup.fr/v1/")
 
     def test_set_endpoint(self):
-        api = api_wrapper.PublicApi("https://covid-api.mmediagroup.fr/v1/")
-        api.set_endpoint("vaccines")
-        self.assertEqual(api.get_endpoint(), "vaccines")
+        self.api_covid_19.set_endpoint("cases")
+        self.assertEqual(self.api_covid_19.get_endpoint(), "cases")
 
     def test_api_url(self):
-        api = api_wrapper.PublicApi("https://covid-api.mmediagroup.fr/v1/")
-        api.set_endpoint("vaccines")
-        self.assertEqual(api.api_url(), "https://covid-api.mmediagroup.fr/v1/vaccines")
+        self.assertEqual(self.api_covid_19.api_url(), "https://covid-api.mmediagroup.fr/v1/vaccines")
+
+
+class TestApiWrapper(unittest.TestCase):
+    pass
